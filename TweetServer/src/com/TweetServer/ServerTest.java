28dedd02ed7c756296aa32e24708fa874e2fd787
Nick Lawler <lawler145@gmail.com>
2015-02-27 11:36:22 -0800

    initial commit

---
+++ TweetServer/src/com/TweetServer/ServerTest.java
@@ -0,0 +1,14 @@
+package com.TweetServer;
+
+import static spark.Spark.*;
+
+public class ServerTest {
+
+	public static void main(String[] args) {
+		get("/hello", (req, res) -> {
+			res.type("application/json");
+			return "{\"yolo\": \"swag\"}";
+		});
+	}
+
+}
